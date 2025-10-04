96520d7900784f24e69659bffe5730ee63b1438e
cyberula <mmontagut256@alumnos.iua.edu.ar>
2025-10-04 12:41:33 -0300

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env bash
+set -euo pipefail
+
+REMOTE="djs.ar"        # SSH host alias from ~/.ssh/config
+TARGET="/home/monsqcgn/djs.ar"
+
+rsync -avz --delete \
+  --exclude=".git" \
+  --exclude=".gitignore" \
+  --exclude="firebase-debug.log" \
+  --exclude="deploy.sh" \
+  ./ "$REMOTE:$TARGET/"
